3799b728b016107bab98ebf70badabc095a0a6fe
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-05-28 19:43:26 +0200

    [config]: change to bare workflow and added oneSignal

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { OneSignal } from 'react-native-onesignal'
 
 import { Routes } from './src/routes';
 
@@ -8,6 +9,9 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize('044fb279-2864-429f-9524-8f16db5b1a29')
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
