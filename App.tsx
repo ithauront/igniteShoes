ff30cf91e321f58e6008bd339529f452f704208d
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-06-01 14:55:41 +0200

    [feta]: listening to button clicks on notification

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { OneSignal } from 'react-native-onesignal'
+import { NotificationClickEvent, OneSignal } from 'react-native-onesignal'
 
 import { Routes } from './src/routes';
 
@@ -10,6 +10,7 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext'
 import { tagUserInfoCreate } from './src/notifications/notificationsTags';
+import { useEffect } from 'react';
 
 OneSignal.initialize('044fb279-2864-429f-9524-8f16db5b1a29')
 OneSignal.Notifications.requestPermission(true)
@@ -18,6 +19,26 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserInfoCreate()
+
+  useEffect(()=>{
+    const handleNotificationClick = (event: NotificationClickEvent):void=>{
+     const {actionId} = event.result
+
+     switch(actionId) {
+      case "1":
+        console.log('clicou no ver todos')
+        break
+      case "2":
+        console.log('clciou no ver pedido')
+        break
+      default: console.log('Nenhum botão clicado')
+      break
+     }
+    }
+     OneSignal.Notifications.addEventListener('click', handleNotificationClick)
+
+     return ()=> OneSignal.Notifications.removeEventListener('click', handleNotificationClick)
+  },[])
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -31,4 +52,4 @@ export default function App() {
       </CartContextProvider>
     </NativeBaseProvider>
   );
-}
+} 
